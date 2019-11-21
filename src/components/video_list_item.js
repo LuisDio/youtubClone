913d9c6cd89e60a999a11f2bcb1536a8712f309f
LuisDio <luis.diolindo@gmail.com>
2019-11-22 01:24:03 +0300

    add video response to videoListItems

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-  //const video = props.video;
+  const imagUrl = video.snippet.thumbnails.default.url;
   return(
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imagUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
   );
 }
 
