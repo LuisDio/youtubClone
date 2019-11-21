e04be028a3f9056a2464fe8b68ad0c19637990ef
LuisDio <luis.diolindo@gmail.com>
2019-11-21 21:05:51 +0300

    transfer func to class compo and init App state with video

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { term: '' }
+    this.state = { term: 'Searching value' }
   }
 
   render() {
@@ -13,7 +13,7 @@ class SearchBar extends Component {
       <div>
         <input
           value={this.state.term}
-           />
+          onChange={event => this.setState({term: event.target.value })} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBtQ2ps2E_et40xm0jsgbnZzLgemUGaYqQ';
@@ -7,12 +8,26 @@ const API_KEY = 'AIzaSyBtQ2ps2E_et40xm0jsgbnZzLgemUGaYqQ';
 // Create a new component. This component should  produce
 // some HTML
 
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { video: []};
+
+    YTSearch({key: API_KEY, term: 'surfboard'}, function(data) {
+      this.setState = ({ video: data });
+    });
+  }
+
+  render() {
+
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 // Take this component generated HTML and
 // put it on the page
